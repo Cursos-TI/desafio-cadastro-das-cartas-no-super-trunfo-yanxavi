43a2a95b680f35b41bc0658003c8c61dad5e8b29
yan xavier <129429365+yanxavi@users.noreply.github.com>
2025-08-11 15:56:59 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,27 +1,6 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de
-// cadastro de cartas de cidades. Siga os comentários para implementar cada
-// parte do desafio.
-// Teste larissa
-
 int main() {
-  // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-  // Exemplos de atributos: código da cidade, nome, população, área, PIB, número
-  // de pontos turísticos.
-
-  // Cadastro das Cartas:
-  // Sugestão: Utilize a função scanf para capturar as entradas do usuário para
-  // cada atributo. Solicite ao usuário que insira as informações de cada
-  // cidade, como o código, nome, população, área, etc.
-
-  // Exibição dos Dados das Cartas:
-  // Sugestão: Utilize a função printf para exibir as informações das cartas
-  // cadastradas de forma clara e organizada. Exiba os valores inseridos para
-  // cada atributo da cidade, um por linha.
-
   // ############################ DECLARAÇÃO DE VARIÁVEIS ############################
 
   // carta 1
@@ -36,25 +15,19 @@ int main() {
 
   // ############################ CADASTRO DA PRIMEIRA CARTA ############################
   printf("==== CADASTRO DA PRIMEIRA CARTA ====\n");
-
+  
   printf("Nome do estado (SEM ESPAÇOS; ex. new-york):\n");
   scanf("%s", estado1);
-
   printf("Código do estado (Abreviatura; ex. NY):\n");
   scanf("%s", codigo1);
-
   printf("Nome da cidadse (SEM ESPAÇOS; ex. nova-iorque):\n");
   scanf("%s", cidade1);
-
   printf("População da cidade (SEM PONTOS OU VÍRGULAS):\n");
   scanf("%d", &populacao1);
-
   printf("PIB:\n");
   scanf("%f", &pib1);
-
   printf("Área em Km²:\n");
   scanf("%f", &area1);
-
   printf("N° de Pontos turísticos:\n");
   scanf("%d", &turismo1);
 
@@ -64,22 +37,16 @@ int main() {
 
   printf("Nome do estado (SEM ESPAÇOS; ex. new-york):\n");
   scanf("%s", estado2);
-
   printf("Código do estado (Abreviatura; ex. NY):\n");
   scanf("%s", codigo2);
-
   printf("Nome da cidade (SEM ESPAÇOS; ex. nova-iorque):\n");
   scanf("%s", cidade2);
-
   printf("População da cidade (SEM PONTOS OU VÍRGULAS):\n");
   scanf("%d", &populacao2);
-
   printf("PIB:\n");
   scanf("%f", &pib2);
-
   printf("Área em Km²:\n");
   scanf("%f", &area2);
-
   printf("N° de Pontos turísticos:\n");
   scanf("%d", &turismo2);
 
